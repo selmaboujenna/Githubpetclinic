76c7e9e19958f8a8f7f350a334effed0738ec320
cloud-init created default user <selmaboujenna@firsyt-VM.da1rzn4amuzudbecubh0ofzpng.ax.internal.cloudapp.net>
2024-05-03 11:58:10 +0000

    terra

---
+++ terraform/main.tf
@@ -0,0 +1,107 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 0.13.4"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_resource_group" "boujenna_selma-rg" {
+  name = "boujenna_selma-rg"
+}
+
+resource "azurerm_virtual_network" "provisioning-vnet" {
+  name                = "${var.resource_prefix}-vnet"
+  address_space       = var.node_addres_space
+  location            = var.node_location
+  resource_group_name = var.node_rgname
+}
+
+resource "azurerm_subnet" "provisioning-subnet" {
+  name                 = "${var.resource_prefix}-subnet"
+  resource_group_name  = var.node_rgname
+  virtual_network_name = azurerm_virtual_network.provisioning-vnet.name
+  address_prefixes     = var.node_address_prefix
+}
+
+resource "azurerm_network_security_group" "provisioning-nsg" {
+  name                = "${var.resource_prefix}-nsg"
+  location            = var.node_location
+  resource_group_name = var.node_rgname
+}
+
+resource "azurerm_network_security_rule" "security_rule" {
+  name                        = "security_rule"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.node_rgname
+  network_security_group_name = azurerm_network_security_group.provisioning-nsg.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "sub_associations" {
+  subnet_id                 = azurerm_subnet.provisioning-subnet.id
+  network_security_group_id = azurerm_network_security_group.provisioning-nsg.id
+}
+
+resource "azurerm_public_ip" "provisioning_public_ip" {
+  count               = var.node_count
+  name                = "${var.resource_prefix}-ipconfig-${count.index}"
+  resource_group_name = var.node_rgname
+  location            = var.node_location
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "provisioning-nic" {
+  count               = var.node_count
+  name                = "${var.resource_prefix}-nic-${count.index}"
+  location            = var.node_location
+  resource_group_name = var.node_rgname
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.provisioning-subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = element(azurerm_public_ip.provisioning_public_ip.*.id, count.index)
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "linux_vm" {
+  count                 = var.node_count
+  name                  = "${var.resource_prefix}-${count.index}"
+  resource_group_name   = var.node_rgname
+  location              = var.node_location
+  size                  = "Standard_F2s_v2"
+  admin_username        = "adminuser"
+  network_interface_ids = [element(azurerm_network_interface.provisioning-nic.*.id, count.index)]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("/home/selmaboujenna/.ssh/azurekey.pub")
+  }
+
+  os_disk {
+    name                 = "myosdisk-${count.index}"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "SUSE"
+    offer     = "openSUSE-leap-15-4"
+    sku       = "gen2"
+    version   = "latest"
+  }
+}
