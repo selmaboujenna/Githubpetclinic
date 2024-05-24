4c13072273062c93031265636bb27af60b715b76
selmaboujenna <selmaboujenna@devoteam.com>
2024-05-24 16:58:19 +0200

    hh

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,9 +15,6 @@ terraform {
     resource_group_name  = "boujenna_selma-rg"                                        
     key                  = "prod.terraform.tfstate"                
     use_msi              = true                                    
-    client_id            = var.client_id  
-    subscription_id      = var.subscription_id 
-    tenant_id            = var.tenant_id 
   }
 }
 
